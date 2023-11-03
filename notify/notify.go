1fb487207cd23b67832a5784136385acc19b6cef
wangxiaowei <wangxiaowei1@lixiang.com>
2023-11-03 22:53:33 +0800

    fix: DedupStage

--- notify/notify.go
+++ notify/notify.go
@@ -673,6 +673,7 @@ func (n *DedupStage) Exec(ctx context.Context, _ log.Logger, alerts ...*types.Al
 
 	ctx = WithFiringAlerts(ctx, firing)
 	ctx = WithResolvedAlerts(ctx, resolved)
+
 	if n.needsUpdate(entry, firingSet, resolvedSet, repeatInterval) {
 		return ctx, alerts, nil
 	}
