6537e5d405d738106c901cb6f78e924db38a27c3
wangxiaowei <wangxiaowei1@lixiang.com>
2024-02-01 10:38:13 +0800

    add log

--- notify/notify.go
+++ notify/notify.go
@@ -407,7 +407,7 @@ func (rs RoutingStage) Exec(ctx context.Context, l log.Logger, alerts ...*types.
 
 	s, ok := rs[receiver]
 	if !ok {
-		return ctx, nil, errors.Errorf("stage [%s] for receiver missing", receiver)
+		return ctx, nil, errors.Errorf("stage for receiver [%s] missing", receiver)
 
 	}
 
