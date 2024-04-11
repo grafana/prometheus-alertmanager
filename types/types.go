aea198bc01728a7f8b32dcb2242c50e797b3076f
wangxiaowei <wangxiaowei1@lixiang.com>
2024-04-11 16:10:14 +0800

    add TriggerAt

--- types/types.go
+++ types/types.go
@@ -297,6 +297,8 @@ func (e *MultiError) Error() string {
 type Alert struct {
 	model.Alert
 	RuleUID string
+	// The threshold timestamp
+	TriggerAt time.Time
 	// The authoritative timestamp.
 	UpdatedAt time.Time
 	Timeout   bool
